522f2c0e20c6414c41b6935de8bce0b6d4f90d58
bmbtnkr <tombanker@gmail.com>
2017-03-27 23:58:48 -0700

    added stretchy ik, knee pinning, improved ik/fk blend performance

--- src/rigLib/blocks/fkIkChain.py
+++ src/rigLib/blocks/fkIkChain.py
@@ -23,11 +23,24 @@ Control Flags: FK[...], IK, Pole Vector, FK/IK Switch
 # chain end = "arm"
 # chain terminator = "arm"
 
+# Features
 # ToDo: make this work with any number of middle joints
 # ToDo: add bendy/rotate joints in between each bone
-# ToDo: make sure hide attrs is working
-# ToDo: make this work with any number of middle joints
-# ToDo: make this work with any number of middle joints
+# ToDo: make sure hide attrs is working - done
+# ToDo: finish ik stretch setup - done
+# ToDo: setup blend color node for result chain, but for translate as well - done
+# ToDo: setup ik knee pinning - done
+# ToDo: connect ik handle twist into new ik ctrl knee twist attribute - done
+# ToDo: add addtional tweak offset control to knee joint
+# ToDo: add soft Ik solver
+
+# Cleanup
+# ToDo: remove dist_ctrl_l_foot_ik_mid_stretch - done
+# ToDo: remove dist_ctrl_l_foot_ik_end_stretch - done
+# ToDo: remove jt_l_thigh_ik_pos_null - done
+# ToDo: connect rootIkJnt to kneePin and stretch matrix1 - done
+# ToDo: knee pinning solve cleanup: when stretch is on use the stretch distance in knee pinning mult
+#                                   when stretch is off, use root to end ik jnt distance in knee pinning mult
 
 import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
@@ -73,28 +86,36 @@ class FkIkChain(object):
         midJntBind = joint.Joint(name=self.joints[1], create=False)
         endJntBind = joint.Joint(name=self.joints[2], create=False)
 
-        self.rootJnt = joint.Joint(name=rootJntBind.name.replace('bind', 'template'),
+        self.rootJnt = joint.Joint(name=rootJntBind.name.replace('bind', 'result'),
                                    translateTo=rootJntBind, rotateTo=rootJntBind,
                                    jointOrient=rootJntBind.get_joint_orient(), jointTag='ikfkChain_root')
-
-        self.midJnt = joint.Joint(name=midJntBind.name.replace('bind', 'template'),
+        self.midJnt = joint.Joint(name=midJntBind.name.replace('bind', 'result'),
                                   translateTo=midJntBind, rotateTo=midJntBind,
                                   jointOrient=midJntBind.get_joint_orient(), jointTag='ikfkChain_mid', parent=self.rootJnt)
-
-        cmds.makeIdentity(self.midJnt, apply=True)
-
-        self.endJnt = joint.Joint(name=endJntBind.name.replace('bind', 'template'),
+        self.endJnt = joint.Joint(name=endJntBind.name.replace('bind', 'result'),
                                   translateTo=endJntBind, rotateTo=endJntBind,
                                   jointOrient=endJntBind.get_joint_orient(), jointTag='ikfkChain_end', parent=self.midJnt)
+        cmds.makeIdentity(self.rootJnt, apply=True)
+
+
+        self.rootJntFk = joint.Joint(name=self.rootJnt.name.replace('result', 'fk'), translateTo=self.rootJnt, rotateTo=self.rootJnt)
+        self.midJntFk = joint.Joint(name=self.midJnt.name.replace('result', 'fk'), translateTo=self.midJnt, rotateTo=self.midJnt, parent=self.rootJntFk)
+        self.endJntFk = joint.Joint(name=self.endJnt.name.replace('result', 'fk'), translateTo=self.endJnt, rotateTo=self.endJnt, parent=self.midJntFk)
+        cmds.makeIdentity(self.rootJntFk, apply=True)
+
+        self.rootJntIk = joint.Joint(name=self.rootJnt.name.replace('result', 'ik'), translateTo=self.rootJnt, rotateTo=self.rootJnt)
+        self.midJntIk = joint.Joint(name=self.midJnt.name.replace('result', 'ik'), translateTo=self.midJnt, rotateTo=self.midJnt, parent=self.rootJntIk)
+        self.endJntIk = joint.Joint(name=self.endJnt.name.replace('result', 'ik'), translateTo=self.endJnt, rotateTo=self.endJnt, parent=self.midJntIk)
+        cmds.makeIdentity(self.rootJntIk, apply=True)
 
         # create fk ctrls
-        self.fkRootCtrl = control.Control(name=self.rootJnt.name.replace('jt', 'ctrl').replace('template', 'fk'),
+        self.fkRootCtrl = control.Control(name=self.rootJnt.name.replace('jt', 'ctrl').replace('result', 'fk'),
                                           translateTo=self.rootJnt, rotateTo=self.rootJnt, normal=(1, 0, 0))
 
-        self.fkMidCtrl = control.Control(name=self.midJnt.name.replace('jt', 'ctrl').replace('template', 'fk'),
+        self.fkMidCtrl = control.Control(name=self.midJnt.name.replace('jt', 'ctrl').replace('result', 'fk'),
                                          translateTo=self.midJnt, rotateTo=self.midJnt, normal=(1, 0, 0))
 
-        self.fkEndCtrl = control.Control(name=self.endJnt.name.replace('jt', 'ctrl').replace('template', 'fk'),
+        self.fkEndCtrl = control.Control(name=self.endJnt.name.replace('jt', 'ctrl').replace('result', 'fk'),
                                          translateTo=self.endJnt, rotateTo=self.endJnt)
 
         self.fkRootCtrl.create_null_grps()
@@ -112,45 +133,144 @@ class FkIkChain(object):
         self.fkMidCtrl.hideChannels(('t', 's', 'v'))
         self.fkEndCtrl.hideChannels(('t', 's', 'v'))
 
-        fkRootOrientConstraint = cmds.orientConstraint(self.fkRootCtrl.name, self.rootJnt)[0]
-        fkMidOrientConstraint = cmds.orientConstraint(self.fkMidCtrl.name, self.midJnt)[0]
-        fkEndOrientConstraint = cmds.orientConstraint(self.fkEndCtrl.name, self.endJnt)[0]
+        fkRootOrientConstraint = cmds.orientConstraint(self.fkRootCtrl.name, self.rootJntFk)[0]
+        fkMidOrientConstraint = cmds.orientConstraint(self.fkMidCtrl.name, self.midJntFk)[0]
+        fkEndOrientConstraint = cmds.orientConstraint(self.fkEndCtrl.name, self.endJntFk)[0]
 
         # create ik ctrls
-        self.ikCtrl = control.Control(name=self.endJnt.name.replace('jt', 'ctrl').replace('template', 'ik'),
-                                      translateTo=self.endJnt, rotateTo=self.endJnt,
-                                      lockAttrs=('r', 's', 'v'), hideAttrs=('r', 's', 'v'))
+        self.ikCtrl = control.Control(name=self.endJnt.name.replace('jt', 'ctrl').replace('result', 'ik'),
+                                      translateTo=self.endJnt)
         self.ikCtrl.create_null_grps()
+        self.ikCtrl.lockChannels(('r', 's', 'v'))
+        self.ikCtrl.hideChannels(('r', 's', 'v'))
         self.ikCtrl.nullGrp.set_scale((2,2,2))
-        cmds.addAttr(self.ikCtrl.name, longName='ikBlend', attributeType='float', defaultValue=1, minValue=0, maxValue=1, keyable=True)
 
-        self.ikHandle = cmds.ikHandle(name='%s_ikHandle' % self.ikCtrl.name, startJoint=self.rootJnt, endEffector=self.endJnt,
+        cmds.addAttr(self.ikCtrl.name, longName='ikBlend', attributeType='float', defaultValue=1, minValue=0, maxValue=1, keyable=True)
+        cmds.addAttr(self.ikCtrl.name, longName='stretch', attributeType='float', defaultValue=0, minValue=0, maxValue=1, keyable=True)
+        cmds.addAttr(self.ikCtrl.name, longName='kneePin', attributeType='float', defaultValue=0, minValue=0, maxValue=1, keyable=True)
+        cmds.addAttr(self.ikCtrl.name, longName='twist', attributeType='float', defaultValue=0, keyable=True)
+
+        self.ikHandle = cmds.ikHandle(name='%s_ikHandle' % self.ikCtrl.name, startJoint=self.rootJntIk, endEffector=self.endJntIk,
                                       solver='ikRPsolver')
 
-        cmds.rename(self.ikHandle[1], '%s_effector' % self.endJnt)
+        cmds.rename(self.ikHandle[1], '%s_eff' % self.endJntIk)
         self.ikHandle = transform.Transform(name=self.ikHandle[0], create=False)
         ikHandleParentConstraint = cmds.parentConstraint(self.ikCtrl.name, self.ikHandle.name)[0]
-        constraintTargets = cmds.parentConstraint(ikHandleParentConstraint, weightAliasList=True, query=True)
-
-        cmds.connectAttr('%s.ikBlend' % self.ikCtrl.name, '%s.ikBlend' % self.ikHandle.name)
-        cmds.connectAttr('%s.ikBlend' % self.ikCtrl.name, '%s.%s' % (ikHandleParentConstraint, constraintTargets[0]))
-
-        fkOrientConstraintReverse = cmds.createNode('reverse', name='rev_%s' % self.fkRootCtrl.name)
-        cmds.connectAttr('%s.ikBlend' % self.ikCtrl.name, '%s.inputX' % fkOrientConstraintReverse)
-
-        for constraint in [fkRootOrientConstraint, fkMidOrientConstraint, fkEndOrientConstraint]:
-            constraintTarget = cmds.orientConstraint(constraint, weightAliasList=True, query=True)[0]
-            cmds.connectAttr('%s.outputX' % fkOrientConstraintReverse, '%s.%s' % (constraint, constraintTarget))
+        cmds.connectAttr('%s.twist' % self.ikCtrl.name, '%s.twist' % self.ikHandle.name)
 
         # create pv ctrl
-        self.ikPvCtrl = control.Control(name=self.midJnt.name.replace('jt', 'ctrl').replace('template', 'pv'),
+        self.ikPvCtrl = control.Control(name=self.midJnt.name.replace('jt', 'ctrl').replace('result', 'pv'),
                                         translate=self.set_pole_vector()[0], rotate=self.set_pole_vector()[1])
+        self.ikPvCtrl.create_null_grps()
         cmds.poleVectorConstraint(self.ikPvCtrl.name, self.ikHandle.name)
+        self.ikPvCtrl.lockChannels(('r', 's', 'v'))
+        self.ikPvCtrl.hideChannels(('r', 's', 'v'))
+
+        # Setup stretchy ik
+        # rootJntIkPosNull = transform.Transform(name='%s_pos_null' % self.rootJntIk, translateTo=self.rootJnt)
+
+        ikStretchMidDistance = cmds.createNode('distanceBetween', name='dist_%s_mid_stretch' % self.ikCtrl.name)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.rootJntIk, '%s.inMatrix1' % ikStretchMidDistance)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.midJntIk, '%s.inMatrix2' % ikStretchMidDistance)
+
+        ikStretchEndDistance = cmds.createNode('distanceBetween', name='dist_%s_end_stretch' % self.ikCtrl.name)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.midJntIk, '%s.inMatrix1' % ikStretchEndDistance)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.endJntIk, '%s.inMatrix2' % ikStretchEndDistance)
+
+        ikStretchCtrlDistance = cmds.createNode('distanceBetween', name='dist_%s_ctrl_stretch' % self.ikCtrl.name)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.rootJntIk, '%s.inMatrix1' % ikStretchCtrlDistance)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.ikCtrl.name, '%s.inMatrix2' % ikStretchCtrlDistance)
+
+        ikStretchFactorMult = cmds.createNode('multiplyDivide', name='div_%s_stretch' % self.ikCtrl.name)
+        cmds.setAttr('%s.operation' % ikStretchFactorMult, 2)
+        cmds.connectAttr('%s.distance' % ikStretchCtrlDistance, '%s.input1X' % ikStretchFactorMult)
+        ikStretchSumDistance = float(cmds.getAttr('%s.distance' % ikStretchMidDistance) + cmds.getAttr('%s.distance' % ikStretchEndDistance))
+        cmds.setAttr('%s.input2X' % ikStretchFactorMult, ikStretchSumDistance)
+
+        ikStretchFactorCond = cmds.createNode('condition', name='cond_%s_stretch' % self.ikCtrl.name)
+        cmds.setAttr('%s.operation' % ikStretchFactorCond, 2)
+        cmds.connectAttr('%s.distance' % ikStretchCtrlDistance, '%s.firstTerm' % ikStretchFactorCond)
+        cmds.setAttr('%s.secondTerm' % ikStretchFactorCond, ikStretchSumDistance)
+        cmds.connectAttr('%s.outputX' % ikStretchFactorMult, '%s.colorIfTrueR' % ikStretchFactorCond)
+
+        ikStretchMidMult = cmds.createNode('multiplyDivide', name='mult_%s_mid_stretch' % self.ikCtrl.name)
+        cmds.connectAttr('%s.outColorR' % ikStretchFactorCond, '%s.input1X' % ikStretchMidMult)
+        cmds.setAttr('%s.input2X' % ikStretchMidMult, cmds.getAttr('%s.distance' % ikStretchMidDistance))
+
+        ikStretchEndMult = cmds.createNode('multiplyDivide', name='mult_%s_end_stretch' % self.ikCtrl.name)
+        cmds.connectAttr('%s.outColorR' % ikStretchFactorCond, '%s.input1X' % ikStretchEndMult)
+        cmds.setAttr('%s.input2X' % ikStretchEndMult, cmds.getAttr('%s.distance' % ikStretchEndDistance))
+
+        ikStretchAttrColor = cmds.createNode('blendColors', name='blend_%s_attr_stretch' % self.ikCtrl.name)
+        cmds.connectAttr('%s.stretch' % self.ikCtrl.name, '%s.blender' % ikStretchAttrColor)
+        cmds.connectAttr('%s.outputX' % ikStretchMidMult, '%s.color1R' % ikStretchAttrColor)
+        cmds.connectAttr('%s.outputX' % ikStretchEndMult, '%s.color1G' % ikStretchAttrColor)
+        cmds.setAttr('%s.color2R' % ikStretchAttrColor, cmds.getAttr('%s.distance' % ikStretchMidDistance))
+        cmds.setAttr('%s.color2G' % ikStretchAttrColor, cmds.getAttr('%s.distance' % ikStretchEndDistance))
+        cmds.delete(ikStretchMidDistance, ikStretchEndDistance)
+
+        # Setup knee pinning
+        ikKneePinRootDistance = cmds.createNode('distanceBetween', name='dist_%s_root_kneePin' % self.ikCtrl.name)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.rootJntIk, '%s.inMatrix1' % ikKneePinRootDistance)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.ikPvCtrl.name, '%s.inMatrix2' % ikKneePinRootDistance)
+
+        ikKneePinEndDistance = cmds.createNode('distanceBetween', name='dist_%s_end_kneePin' % self.ikCtrl.name)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.ikPvCtrl.name, '%s.inMatrix1' % ikKneePinEndDistance)
+        cmds.connectAttr('%s.worldMatrix[0]' % self.ikCtrl.name, '%s.inMatrix2' % ikKneePinEndDistance)
+
+        ikKneePinAttrColor = cmds.createNode('blendColors', name='blend_%s_attr_kneePin' % self.ikCtrl.name)
+        cmds.connectAttr('%s.kneePin' % self.ikCtrl.name, '%s.blender' % ikKneePinAttrColor)
+        cmds.connectAttr('%s.distance' % ikKneePinRootDistance, '%s.color1R' % ikKneePinAttrColor)
+        cmds.connectAttr('%s.distance' % ikKneePinEndDistance, '%s.color1G' % ikKneePinAttrColor)
+        cmds.connectAttr('%s.outputR' % ikStretchAttrColor, '%s.color2R' % ikKneePinAttrColor)
+        cmds.connectAttr('%s.outputG' % ikStretchAttrColor, '%s.color2G' % ikKneePinAttrColor)
+        cmds.connectAttr('%s.outputR' % ikKneePinAttrColor, '%s.translateX' % self.midJntIk)
+        cmds.connectAttr('%s.outputG' % ikKneePinAttrColor, '%s.translateX' % self.endJntIk)
+
+        # Setup ik/fk-to-result chain blend
+        thighResultPosBlendColors = cmds.createNode('blendColors', name='blend_%s_pos' % self.rootJnt.name)
+        kneeResultPosBlendColors = cmds.createNode('blendColors', name='blend_%s_pos' % self.midJnt.name)
+        ankleResultPosBlendColors = cmds.createNode('blendColors', name='blend_%s_pos' % self.endJnt.name)
+
+        cmds.connectAttr('%s.translate' % self.rootJntIk, '%s.color1' % thighResultPosBlendColors)
+        cmds.connectAttr('%s.translate' % self.rootJntFk, '%s.color2' % thighResultPosBlendColors)
+        cmds.connectAttr('%s.output' % thighResultPosBlendColors, '%s.translate' % self.rootJnt)
+        cmds.connectAttr('%s.ikBlend' % self.ikCtrl.name, '%s.blender' % thighResultPosBlendColors)
+
+        cmds.connectAttr('%s.translate' % self.midJntIk, '%s.color1' % kneeResultPosBlendColors)
+        cmds.connectAttr('%s.translate' % self.midJntFk, '%s.color2' % kneeResultPosBlendColors)
+        cmds.connectAttr('%s.output' % kneeResultPosBlendColors, '%s.translate' % self.midJnt)
+        cmds.connectAttr('%s.ikBlend' % self.ikCtrl.name, '%s.blender' % kneeResultPosBlendColors)
+
+        cmds.connectAttr('%s.translate' % self.endJntIk, '%s.color1' % ankleResultPosBlendColors)
+        cmds.connectAttr('%s.translate' % self.endJntFk, '%s.color2' % ankleResultPosBlendColors)
+        cmds.connectAttr('%s.output' % ankleResultPosBlendColors, '%s.translate' % self.endJnt)
+        cmds.connectAttr('%s.ikBlend' % self.ikCtrl.name, '%s.blender' % ankleResultPosBlendColors)
+
+        thighResultRotBlendColors = cmds.createNode('blendColors', name='blend_%s_rot' % self.rootJnt.name)
+        kneeResultRotBlendColors = cmds.createNode('blendColors', name='blend_%s_rot' % self.midJnt.name)
+        ankleResultRotBlendColors = cmds.createNode('blendColors', name='blend_%s_rot' % self.endJnt.name)
+
+        cmds.connectAttr('%s.rotate' % self.rootJntIk, '%s.color1' % thighResultRotBlendColors)
+        cmds.connectAttr('%s.rotate' % self.rootJntFk, '%s.color2' % thighResultRotBlendColors)
+        cmds.connectAttr('%s.output' % thighResultRotBlendColors, '%s.rotate' % self.rootJnt)
+        cmds.connectAttr('%s.ikBlend' % self.ikCtrl.name, '%s.blender' % thighResultRotBlendColors)
+
+        cmds.connectAttr('%s.rotate' % self.midJntIk, '%s.color1' % kneeResultRotBlendColors)
+        cmds.connectAttr('%s.rotate' % self.midJntFk, '%s.color2' % kneeResultRotBlendColors)
+        cmds.connectAttr('%s.output' % kneeResultRotBlendColors, '%s.rotate' % self.midJnt)
+        cmds.connectAttr('%s.ikBlend' % self.ikCtrl.name, '%s.blender' % kneeResultRotBlendColors)
+
+        cmds.connectAttr('%s.rotate' % self.endJntIk, '%s.color1' % ankleResultRotBlendColors)
+        cmds.connectAttr('%s.rotate' % self.endJntFk, '%s.color2' % ankleResultRotBlendColors)
+        cmds.connectAttr('%s.output' % ankleResultRotBlendColors, '%s.rotate' % self.endJnt)
+        cmds.connectAttr('%s.ikBlend' % self.ikCtrl.name, '%s.blender' % ankleResultRotBlendColors)
+
 
     def set_pole_vector(self):
-        startV = apiUtils.set_mVector(self.rootJnt.get_translation())
-        midV = apiUtils.set_mVector(self.midJnt.get_translation())
-        endV = apiUtils.set_mVector(self.endJnt.get_translation())
+        startV = apiUtils.set_mVector(self.rootJntIk.get_translation())
+        midV = apiUtils.set_mVector(self.midJntIk.get_translation())
+        endV = apiUtils.set_mVector(self.endJntIk.get_translation())
 
         startEnd = endV - startV
         startMid = midV - startV
@@ -159,7 +279,7 @@ class FkIkChain(object):
         startEndN = startEnd.normal()
         projV = startEndN * proj
         arrowV = startMid - projV
-        arrowV *= 10
+        arrowV *= 2
         finalV = arrowV + midV
         pos = (finalV.x, finalV.y, finalV.z)
 
